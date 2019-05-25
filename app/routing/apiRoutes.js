970fc7146ca56a4c7013443f5774a1afe51f6668
Patience D <patience.deaton2@outlook.com>
2019-05-24 23:30:35 -0400

    fixed bugs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,18 +6,15 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-
-        debugger;
-        let results = req.body;
+        let results = req.body.scores;
         for (let i = 0; i < results.length; i++) {
-            results[i] = parseInt(results[i]);
+            results[i] = parseInt(results[i].split(" ")[0]);
         }
-        let bestCandidate = 0;
+        let bestCandidate;
         let bestDiff = Number.MAX_SAFE_INTEGER;
 
         for (i = 0; i < randos.length; i++) {
             var tempDiff = calcDiff(results, randos[i].scores);
-
             console.log("The Diff between", results, "and", randos[i].name, randos[i].scores, "is", tempDiff);
             if (tempDiff < bestDiff) {
                 bestDiff = tempDiff;
@@ -26,9 +23,7 @@ module.exports = function (app) {
         }
 
         function calcDiff(userInput, randoFriendInput) {
-
             var DiffAmount = 0;
-
             for (var i = 0; i < userInput.length; i++) {
                 DiffAmount += Math.abs(userInput[i] - randoFriendInput[i]);
             }
